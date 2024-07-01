2ce351c7f4f867d86316bfadb6956a0987a9e431
shehryar_khan <shehryarkhan@gosaas.io>
2024-07-01 11:16:44 +0500

    wrap up dark mode functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react'
 import React from 'react'
 import randomImage from '../assets/random.jpg'
+import ColorModeSwitch from './ColorModeSwitch'
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={'space-between'} padding={'15px'}>
         <Image src={randomImage}  boxSize={'100px'}/>
-        <Text>Navbar</Text>
+        <ColorModeSwitch />
     </HStack>
   )
 }
