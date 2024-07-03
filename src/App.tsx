5a3864d294710326ed3597790fada711dc997dc8
shehryar_khan <shehryarkhan@gosaas.io>
2024-07-03 12:40:03 +0500

    fetch games hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
-import Navbar from './components/Navbar'
+import React from "react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import GameGrid from "./components/GameGrid";
 
 const App = () => {
-  return (
-    <Grid templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`
-    }}>
-      <GridItem area = 'nav'>
-        <Navbar></Navbar>
-      </GridItem>
-      <Show above='lg'>
-        <GridItem area='aside'>
-          Aside
-        </GridItem>
-      </Show>
-      <GridItem area='main'>
-        Main
-      </GridItem>
-    </Grid>
-  )
-}
+    return (
+        <Grid
+            templateAreas={{
+                base: `"nav" "main"`,
+                lg: `"nav nav" "aside main"`,
+            }}
+        >
+            <GridItem area="nav">
+                <Navbar></Navbar>
+            </GridItem>
+            <Show above="lg">
+                <GridItem area="aside">Aside</GridItem>
+            </Show>
+            <GridItem area="main">
+                <GameGrid />
+            </GridItem>
+        </Grid>
+    );
+};
 
-export default App
+export default App;
