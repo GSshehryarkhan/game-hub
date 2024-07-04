9114d688a6fea59988b4a5edbdaa8459b6911177
shehryar_khan <shehryarkhan@gosaas.io>
2024-07-04 12:41:40 +0500

    handle no game img

--- src/services/image-module.ts
+++ src/services/image-module.ts
@@ -1,5 +1,7 @@
+import placeholder_image from "../assets/27002.jpg";
+
 const getCroppedUrl = (url: string) => {
-    if (!url) return "";
+    if (!url) return placeholder_image;
     const target = "media/";
     const index = url.indexOf("media/") + "media/".length;
     return url.slice(0, index) + "crop/600/400/" + url.slice(index);
