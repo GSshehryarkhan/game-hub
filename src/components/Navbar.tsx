cdcc3f7b166f13a2da6dd9ad101503397d7bf5ae
shehryar_khan <shehryarkhan@gosaas.io>
2024-07-04 12:46:57 +0500

    basic search bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,17 @@
-import { HStack, Image } from '@chakra-ui/react'
-import React from 'react'
-import randomImage from '../assets/random.jpg'
-import ColorModeSwitch from './ColorModeSwitch'
+import { HStack, Image } from "@chakra-ui/react";
+import React from "react";
+import randomImage from "../assets/random.jpg";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const Navbar = () => {
-  return (
-    <HStack justifyContent={'space-between'} padding={'15px'}>
-        <Image src={randomImage}  boxSize={'100px'}/>
-        <ColorModeSwitch />
-    </HStack>
-  )
-}
+    return (
+        <HStack padding={"15px"}>
+            <Image src={randomImage} boxSize={"100px"} />
+            <SearchInput />
+            <ColorModeSwitch />
+        </HStack>
+    );
+};
 
-export default Navbar
+export default Navbar;
